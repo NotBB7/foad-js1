01a7aa49af059ebdc8f7cbd40d43e9cd9a70f0f6
NotBB <notice76610@gmail.com>
2023-04-28 13:30:16 +0200

    Update 3.js

--- js/3.js
+++ js/3.js
@@ -1,6 +1,8 @@
+//Création de la variable nb1 et nb2
 let nb1 = 3;
 let nb2 = 6;
 
+//Utilisation de la boucle while
 while (nb1 + nb2 <= 2023){
     //Affiche dans la console
     console.log("La somme est : " + (nb1 + nb2));
