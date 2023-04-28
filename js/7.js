7d6442c1c788f6232de661076e32db11e3338af6
NotBB <notice76610@gmail.com>
2023-04-28 13:30:23 +0200

    Update 7.js

--- js/7.js
+++ js/7.js
@@ -1,6 +1,7 @@
 //Affiche dans la console
 console.log("Start !!");
 
+//Utilisation d'une boucle avec la variable F
 for (let f = 20; f >= 0; f--){
     //Affiche dans la console
     console.log(f);
