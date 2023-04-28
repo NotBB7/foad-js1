9ec695be971ab23d927a6ee3b69ba76ba41ec29e
NotBB <notice76610@gmail.com>
2023-04-28 13:26:51 +0200

    Update 4.js

--- js/4.js
+++ js/4.js
@@ -1,7 +1,9 @@
 for (let c = 1; c <= 10; c++) {
     if (c === 10) {
+    //Affiche dans la console
       console.log("Message " + c + ", Voici le dernier message !");
     } else {
+    //Affiche dans la console
       console.log("Message " + c);
     }
   }
