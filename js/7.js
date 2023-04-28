4492aff1a9425772e9572ee4dd5ac7ada8864b9d
NotBB <notice76610@gmail.com>
2023-04-28 13:26:58 +0200

    Update 7.js

--- js/7.js
+++ js/7.js
@@ -1,7 +1,9 @@
+//Affiche dans la console
 console.log("Start !!");
 
 for (let f = 20; f >= 0; f--){
+    //Affiche dans la console
     console.log(f);
 }
-
+//Affiche dans la console
 console.log("Finish !!");
