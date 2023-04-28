29ff7ab49e37919b25fdb25ce7d5be18bb4fc533
NotBB <notice76610@gmail.com>
2023-04-28 13:16:25 +0200

    Create 4.js

---
+++ js/4.js
@@ -0,0 +1,8 @@
+for (let c = 1; c <= 10; c++) {
+    if (c === 10) {
+      console.log("Message " + c + ", Voici le dernier message !");
+    } else {
+      console.log("Message " + c);
+    }
+  }
+  
